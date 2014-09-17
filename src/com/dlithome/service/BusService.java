fa537cf1a50aeff47c75869c164b1a45a62cdeb3
jerrywang1981 <wangjianjun@gmail.com>
2014-09-17 16:35:02 +0800

    ds

--- src/com/dlithome/service/BusService.java
+++ src/com/dlithome/service/BusService.java
@@ -8,6 +8,30 @@ package com.dlithome.service;
  *
  */
 public class BusService implements ServiceInterface {
+	
+	private String[] ithome = {"6:15","6:45","7:15","7:45","8:15", "9:15","10:15","11:15","12:15","13:15","14:15","15:15",
+			"16:00","16:30","17:00","17:30","18:00","18:30","19:00","19:30","20:00"};
+	private String[] baihe = {"6:00",
+								"6:30",
+								"7:00",
+								"7:30",
+								"8:00",
+								"9:00",
+								"10:00",
+								"11:00",
+								"12:00",
+								"13:00",
+								"14:00",
+								"15:00",
+								"15:30",
+								"16:15",
+								"16:45",
+								"17:15",
+								"17:45",
+								"18:15",
+								"18:45",
+								"19:15",
+								"19:45"};
 
 	/**
 	 * 
@@ -24,6 +48,11 @@ public class BusService implements ServiceInterface {
 		StringBuffer buffer = new StringBuffer(); 
 		if (content.equals("2")) {
 			buffer.append("--百合发车---小区发车--").append("\n\n");
+			
+			for (int i = 0; i < ithome.length; i++) {
+				buffer.append("---" + baihe[i] + "-------" + ithome[i] + "-----");
+			}
+			/*
 			buffer.append("---6:00--------6:15-----").append("\n");
 			buffer.append("---6:30--------6:45-----").append("\n");
 			buffer.append("---7:00--------7:15-----").append("\n");
@@ -45,7 +74,7 @@ public class BusService implements ServiceInterface {
 			buffer.append("---18:45-------19:00----").append("\n");
 			buffer.append("---19:15-------19:30----").append("\n");
 			buffer.append("---19:45-------20:00----").append("\n\n");
-			
+			*/
 			buffer.append("--排队上车--文明乘车--").append("\n");
 		}
 		return buffer.toString();
